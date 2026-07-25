bc80c40ee7e7dc704e7dc82b4fe2a1e1a5bf63ca
Nastya Zhuchkova <karamelkaaaa39@gmail.com>
2026-07-25 09:59:24 +0300

    Fix

--- src/test/java/OrderTestParameterized.java
+++ src/test/java/OrderTestParameterized.java
@@ -2,7 +2,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import pages.MainPage;
 import org.junit.After;
 import org.junit.Before;
